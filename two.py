c4fba6028e0749df514db23f7fbf9d6b83f0732e
Zuhaib Shah <2016n1326@gmail.com>
2020-01-17 01:25:57 +0500

    solved question 2

---
+++ two.py
@@ -0,0 +1,12 @@
+first = 1
+second = 1
+fib = first + second
+total = 0
+
+while fib <= 4000000:
+	if fib % 2 == 0:
+		total += fib
+	first = second
+	second = fib
+	fib = first + second
+print(total)
