9812b140e4e3352a819e2907a15a5db87c1fd28b
Zuhaib Shah <2016n1326@gmail.com>
2021-12-30 22:41:38 +0500

    improves the solution

--- 9/nine.cpp
+++ 9/nine.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int pythagoreanTripletProduct(int sum) {
 	int c;
-	for (int a = 1; a < sum ; a++){
+	for (int a = 1; a < sum /2 ; a++){
 		for (int b = a + 1; b < sum; b++){
 			c = sum - a - b;
 			if ( pow(a,2) + pow(b,2) == pow(c,2) ){
