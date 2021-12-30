3c4ae31e7821027d38e3aca93cdc28980cca7db9
Zuhaib Shah <2016n1326@gmail.com>
2021-12-30 22:38:17 +0500

    restructures project files; solves question 9

---
+++ 9/nine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int pythagoreanTripletProduct(int sum) {
+	int c;
+	for (int a = 1; a < sum ; a++){
+		for (int b = a + 1; b < sum; b++){
+			c = sum - a - b;
+			if ( pow(a,2) + pow(b,2) == pow(c,2) ){
+				return a*b*c;
+			}
+		}
+	}
+}
+
+
+
+
+
+int main() {
+	cout<<pythagoreanTripletProduct(1000);
+
+	return 0;
+}
